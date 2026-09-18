838bdc6cce1f5395e34385fc530fb586e3c00a80
ajrasane <131806219+ajrasane@users.noreply.github.com>
2026-09-18 05:04:51 +0000

    Split ONNX Runtime patching capabilities
    Signed-off-by: ajrasane <131806219+ajrasane@users.noreply.github.com>

---
+++ modelopt/onnx/quantization/ort_patches.py
@@ -0,0 +1,120 @@
+# Adapted from https://github.com/microsoft/onnxruntime/blob/baeece44ba075009c6bfe95891a8c1b3d4571cb3/onnxruntime/python/tools/quantization/quant_utils.py
+# and https://github.com/microsoft/onnxruntime/blob/baeece44ba075009c6bfe95891a8c1b3d4571cb3/onnxruntime/python/tools/quantization/calibrate.py
+# and https://github.com/microsoft/onnxruntime/blob/2ac381c55397dffff327cc6efecf6f95a70f90a1/onnxruntime/python/tools/quantization/onnx_quantizer.py
+#
+# MIT License
+#
+# Copyright (c) Microsoft Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0 AND MIT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Composition of ModelOpt's ONNX Runtime monkey patches."""
+
+__all__ = []
+
+from onnxruntime.quantization import calibrate, qdq_quantizer
+from onnxruntime.quantization.base_quantizer import BaseQuantizer
+from onnxruntime.quantization.calibrate import (
+    CalibraterBase,
+    HistogramCalibrater,
+    HistogramCollector,
+    MinMaxCalibrater,
+)
+from onnxruntime.quantization.qdq_quantizer import QDQQuantizer
+
+from modelopt.onnx.logging_config import logger
+from modelopt.onnx.quantization.ort_calibration import (
+    _collect_absolute_value,
+    _collect_data_histogram_calibrator,
+    _collect_data_minmax_calibrator,
+    _collect_value,
+    _compute_data_minmax_calibrator,
+    _merge_range_minmax_calibrator,
+)
+from modelopt.onnx.quantization.ort_calibration_per_node import (
+    _augment_graph_histogram_calibrater_single_node_calibration,
+    _augment_graph_min_max_calibrater_single_node_calibration,
+    _collect_data_histogram_calibrater_single_node_calibration,
+    _collect_data_min_max_calibrater_single_node_calibration,
+    _collect_histogram_collector_single_node_calibration,
+    _collect_value_histogram_collector_single_node_calibration,
+    _compute_data_min_max_calibrater_single_node_calibration,
+    _merge_range_min_max_calibrater_single_node_calibration,
+)
+from modelopt.onnx.quantization.ort_quantization import (
+    _adjust_tensor_ranges,
+    _check_opset_version,
+    _compute_scale_zp,
+    _create_calibrator_with_extra_options,
+)
+from modelopt.onnx.quantization.ort_session import (
+    _create_inference_session_with_ep_config,
+    _init_calibrater_base,
+    _select_tensors_to_calibrate,
+)
+
+
+def patch_ort_modules(calibrate_per_node: bool = False):
+    """Patches the ORT modules."""
+    logger.debug("Patching ORT modules")
+    if calibrate_per_node:
+        MinMaxCalibrater.augment_graph = _augment_graph_min_max_calibrater_single_node_calibration
+        MinMaxCalibrater.collect_data = _collect_data_min_max_calibrater_single_node_calibration
+        MinMaxCalibrater.compute_data = _compute_data_min_max_calibrater_single_node_calibration
+        MinMaxCalibrater.merge_range = _merge_range_min_max_calibrater_single_node_calibration
+        HistogramCalibrater.augment_graph = (
+            _augment_graph_histogram_calibrater_single_node_calibration
+        )
+        HistogramCalibrater.collect_data = (
+            _collect_data_histogram_calibrater_single_node_calibration
+        )
+        HistogramCollector.collect = _collect_histogram_collector_single_node_calibration
+        HistogramCollector.collect_value = (
+            _collect_value_histogram_collector_single_node_calibration
+        )
+    else:
+        HistogramCollector.collect_value = _collect_value
+        HistogramCollector.collect_absolute_value = _collect_absolute_value
+        MinMaxCalibrater.compute_data = _compute_data_minmax_calibrator
+        MinMaxCalibrater.collect_data = _collect_data_minmax_calibrator
+        MinMaxCalibrater.merge_range = _merge_range_minmax_calibrator
+        HistogramCalibrater.collect_data = _collect_data_histogram_calibrator
+
+    calibrate.create_calibrator = _create_calibrator_with_extra_options
+    CalibraterBase.create_inference_session = _create_inference_session_with_ep_config
+    CalibraterBase.select_tensors_to_calibrate = _select_tensors_to_calibrate
+    QDQQuantizer.check_opset_version = _check_opset_version
+    BaseQuantizer.adjust_tensor_ranges = _adjust_tensor_ranges
+    qdq_quantizer.compute_scale_zp = _compute_scale_zp
+    CalibraterBase.__init__ = _init_calibrater_base
